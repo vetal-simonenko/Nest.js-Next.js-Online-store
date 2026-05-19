f3757b39e2dec1a2708022b0ace1f64d8f224fb3
VitaliiSimonenko <Vitalii_Simonenko@epam.com>
2026-05-19 12:08:03 +0300

    Commit message

--- src/users/middleware/logger.middleware.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable, NestMiddleware } from '@nestjs/common';
-
-@Injectable()
-export class LoggerMiddleware implements NestMiddleware {
-  use(req: Request, res: Response, next: (error?: any) => void): any {
-    console.log(`[${req.method}] ${req.url}]`);
-    next();
-  }
-}
