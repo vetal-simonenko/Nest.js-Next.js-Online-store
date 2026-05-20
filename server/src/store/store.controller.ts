a2ed20539b9cf00d532eaf1fb96f3b26476c4a87
VitaliiSimonenko <Vitalii_Simonenko@epam.com>
2026-05-20 11:40:05 +0300

    Commit message

--- server/src/store/store.controller.ts
+++ server/src/store/store.controller.ts
@@ -1,7 +1,67 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  Param,
+  Post,
+  Put,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { StoreService } from './store.service';
+import { Auth } from '../auth/decorators/auth.decorators';
+import { CurrentUser } from '../user/decorators/user.decorator';
+import { CreateStoreDto } from './dto/create-store.dto';
+import { UpdateStoreDto } from './dto/update-store.dto';
 
-@Controller('store')
+@Controller('stores')
 export class StoreController {
   constructor(private readonly storeService: StoreService) {}
+
+  @Auth()
+  @Get()
+  async getAll(@CurrentUser('id') userId: string) {
+    return this.storeService.getAll(userId);
+  }
+
+  @Auth()
+  @Get('by-id/:id')
+  async getById(
+    @Param('id') storeId: string,
+    @CurrentUser('id') userId: string,
+  ) {
+    return this.storeService.getByID(storeId, userId);
+  }
+
+  @UsePipes(new ValidationPipe())
+  @HttpCode(200)
+  @Auth()
+  @Post()
+  async create(@CurrentUser('id') userId: string, @Body() dto: CreateStoreDto) {
+    return this.storeService.create(userId, dto);
+  }
+
+  @UsePipes(new ValidationPipe())
+  @HttpCode(200)
+  @Auth()
+  @Put(':id')
+  async update(
+    @Param('id') storeId: string,
+    @CurrentUser('id') userId: string,
+    @Body() dto: UpdateStoreDto,
+  ) {
+    return this.storeService.update(storeId, userId, dto);
+  }
+
+  @HttpCode(200)
+  @Auth()
+  @Delete(':id')
+  async delete(
+    @Param('id') storeId: string,
+    @CurrentUser('id') userId: string,
+  ) {
+    return this.storeService.delete(storeId, userId);
+  }
 }
--- server/src/store/store.service.ts
+++ server/src/store/store.service.ts
@@ -1,7 +1,62 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from '../prisma.service';
+import { Store } from '../../generated/prisma/client';
+import { CreateStoreDto } from './dto/create-store.dto';
+import { UpdateStoreDto } from './dto/update-store.dto';
 
 @Injectable()
 export class StoreService {
   constructor(private readonly prisma: PrismaService) {}
+
+  async getAll(userId: string) {
+    return this.prisma.store.findMany({
+      where: {
+        userId,
+      },
+      orderBy: {
+        createdAt: 'desc',
+      },
+    });
+  }
+
+  async getByID(storeId: string, userId: string) {
+    const store = await this.prisma.store.findUnique({
+      where: { id: storeId, userId: userId },
+    });
+
+    if (!store) {
+      throw new NotFoundException('Store not found');
+    }
+
+    return store;
+  }
+
+  async create(userId: string, dto: CreateStoreDto) {
+    return this.prisma.store.create({
+      data: {
+        ...dto,
+        userId: userId,
+      },
+    });
+  }
+
+  async update(storeId: string, userId: string, dto: UpdateStoreDto) {
+    await this.getByID(storeId, userId);
+
+    return this.prisma.store.update({
+      where: { id: storeId, userId: userId },
+      data: {
+        ...dto,
+        userId: userId,
+      },
+    });
+  }
+
+  async delete(storeId: string, userId: string) {
+    await this.getByID(storeId, userId);
+
+    return this.prisma.store.delete({
+      where: { id: storeId },
+    });
+  }
 }
